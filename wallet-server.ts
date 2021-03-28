b60e79039650ac8b7a181078799f243d6f64c250
leobelizquierdo <leobel.izquierdo@onetree.com>
2021-03-28 19:47:13 -0300

    initial commit

---
+++ wallet-server.ts
@@ -0,0 +1,22 @@
+import { WalletsApi, NetworkApi } from './api';
+import { Configuration } from './configuration';
+
+export class WalletServer {
+	networkApi: NetworkApi;
+	private constructor(protected url: string){
+		let config : Configuration = {
+			basePath: url
+		}
+
+		this.networkApi = new NetworkApi(config);
+	}
+
+	static init(url: string): WalletServer {
+		return new WalletServer(url);
+	}
+
+	async getNetworkInformation(){
+		const res = await this.networkApi.getNetworkInformation();
+		return res.data;
+	}
+}
