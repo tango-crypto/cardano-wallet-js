9028c9fc4f0ac87dbbe7a2eaecd2f82be6d3c77d
Leobel Izquierdo <leobelizquierdo@gmail.com>
2021-03-29 09:59:26 -0300

    add get shelley wallet function

--- test/wallet.ts
+++ test/wallet.ts
@@ -7,18 +7,27 @@ import { WalletServer } from '../wallet-server';
 describe('Cardano wallet API', function() {
 		describe('wallet', function() {
 			let walletServer = WalletServer.init('http://localhost:8090/v2');
-				it("should create/resotre a wallet", async function() {
-					let id = '16f129e025b97f907a760a4cf7b0740d7b4e7993';
-					let name = "empty-balance";
-					let passphrase = '1234567890';
-					let recoverPhrase = ["joy","private","elder","ocean","mobile","orient","arrest","assume","monkey","once","thought","like","warfare","spread","stable",];
-					
-					let wallet = await walletServer.createOrGetShelleyWallet(name, recoverPhrase, passphrase);
-					expect(wallet).be.a("object");
-					expect(wallet).have.property('id').equal(id);
-					expect(wallet).have.property('passphrase').with.property('last_updated_at').be.a('string');
-					expect(wallet).have.property('name').equal(name);
-				});
+			it("should create/resotre a wallet", async function() {
+				let name = "empty-balance";
+				let passphrase = '1234567890';
+				let recoverPhrase = ["joy","private","elder","ocean","mobile","orient","arrest","assume","monkey","once","thought","like","warfare","spread","stable",];
+				
+				let wallet = await walletServer.createOrGetShelleyWallet(name, recoverPhrase, passphrase);
+				expect(wallet).be.a("object");
+				expect(wallet).have.property('id').lengthOf(40);
+				expect(wallet).have.property('passphrase').with.property('last_updated_at').be.a('string');
+				expect(wallet).have.property('name').equal(name);
+			});
+
+			it("should get a wallet", async function() {
+				let id = '16f129e025b97f907a760a4cf7b0740d7b4e7993';
+				
+				let wallet = await walletServer.getShelleyWallet(id);
+				expect(wallet).be.a("object");
+				expect(wallet).have.property('id').equal(id);
+				expect(wallet).have.property('name').be.a('string');
+				expect(wallet).have.property('passphrase').with.property('last_updated_at').be.a('string');
+			});
 
 		});
 });
