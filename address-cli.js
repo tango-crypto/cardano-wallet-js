fb5a1ee1b4055d5c12ab520289da55bfa98f9a6a
Leobel Izquierdo <leobelizquierdo@gmail.com>
2021-04-02 18:03:39 -0300

    fix dependencies on install

--- address-cli.js
+++ address-cli.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
 const { execSync } = require('child_process'); 
-execSync('mkdir -p tmp/cardano-address', {stdio: 'inherit'});
+execSync('mkdir -p tmp/cardano-address');
+execSync('mkdir -p cli');
 execSync('cd tmp/cardano-address && curl -L https://github.com/input-output-hk/cardano-addresses/releases/download/3.2.0/cardano-addresses-3.2.0-linux64.tar.gz -o cardano-addresses-3.2.0-linux64.tar.gz && tar -xf cardano-addresses-3.2.0-linux64.tar.gz', {stdio: 'inherit'});
-execSync('cp tmp/cardano-address/bin/* cli/', {stdio: 'inherit'});
+execSync('cp tmp/cardano-address/bin/* cli/');
 execSync('chmod +x cli/*');
-execSync('rm -rf tmp/cardano-address', {stdio: 'inherit'});
+execSync('rm -rf tmp/cardano-address');
--- utils.ts
+++ utils.ts
@@ -1,9 +1,9 @@
 import { spawnSync } from 'child_process';
 
-const cliPath = 'cli/cardano-address';
+const command = 'cardano-address';
 export class Seed {
 	static generateRecoveryPhrase(size: number = 15): string {
-		const ls = spawnSync(cliPath, ['recovery-phrase', 'generate', '--size', size.toString()], {});
+		const ls = spawnSync(command, ['recovery-phrase', 'generate', '--size', size.toString()], {});
 		return new TextDecoder().decode(ls.stdout).replace(/\n/, '');
 	}
 
