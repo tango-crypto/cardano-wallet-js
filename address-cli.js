03647a5c6dcc809b638ab2fdf45218cbddedc52b
Leobel Izquierdo <leobelizquierdo@gmail.com>
2021-05-30 23:26:33 -0300

    include binaries statically

--- address-cli.js
+++ address-cli.js
@@ -13,14 +13,14 @@ if (os.platform() !== 'win32') {
 	execSync('mkdir tmp-cardano-address');
 	execSync('cd tmp-cardano-address && curl -L https://github.com/input-output-hk/cardano-addresses/releases/download/3.4.0/cardano-addresses-3.4.0-win64.zip -o cardano-addresses-3.4.0-win64.zip && tar -xf cardano-addresses-3.4.0-win64.zip', { stdio: 'inherit' });
 	execSync('copy tmp-cardano-address\\cardano-address.exe cli');
-	execSync('echo @ECHO off> node_modules\\.bin\\cardano-address.exe.cmd');
-	execSync('echo SETLOCAL>> node_modules\\.bin\\cardano-address.exe.cmd');
-	execSync('echo CALL :find_dp0>> node_modules\\.bin\\cardano-address.exe.cmd');
-	execSync('echo ^"%dp0%\\node_modules\\cardano-wallet-js\\cli\\cardano-address.exe^"   %*>> node_modules\\.bin\\cardano-address.exe.cmd');
-	execSync('echo ENDLOCAL>> node_modules\\.bin\\cardano-address.exe.cmd');
-	execSync(`echo EXIT /b ^%errorlevel^%>> node_modules\\.bin\\cardano-address.exe.cmd`);
-	execSync('echo :find_dp0>> node_modules\\.bin\\cardano-address.exe.cmd');
-	execSync('echo SET dp0=%~dp0>> node_modules\\.bin\\cardano-address.exe.cmd');
-	execSync('echo EXIT /b>> node_modules\\.bin\\cardano-address.exe.cmd');
+	execSync('echo @ECHO off> node_modules\\.bin\\cardano-address.cmd');
+	execSync('echo SETLOCAL>> node_modules\\.bin\\cardano-address.cmd');
+	execSync('echo CALL :find_dp0>> node_modules\\.bin\\cardano-address.cmd');
+	execSync('echo ^"%dp0%\\node_modules\\cardano-wallet-js\\cli\\cardano-address.exe^"   %*>> node_modules\\.bin\\cardano-address.cmd');
+	execSync('echo ENDLOCAL>> node_modules\\.bin\\cardano-address.cmd');
+	execSync(`echo EXIT /b ^%errorlevel^%>> node_modules\\.bin\\cardano-address.cmd`);
+	execSync('echo :find_dp0>> node_modules\\.bin\\cardano-address.cmd');
+	execSync('echo SET dp0=%~dp0>> node_modules\\.bin\\cardano-address.cmd');
+	execSync('echo EXIT /b>> node_modules\\.bin\\cardano-address.cmd');
 	execSync('rmdir /s /q tmp-cardano-address');
 }
